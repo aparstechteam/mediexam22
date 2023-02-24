e38aee2c6d373e9519e6be9b01d9ab8340bc4c12
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-02-24 14:32:30 +0600

    updated access

--- assets/lodash.cb5b7d8f.js
+++ assets/lodash.cb5b7d8f.js
@@ -1,4 +1,4 @@
-import{M as jt}from"./index.a6da0124.js";var Ri={exports:{}};/**
+import{M as jt}from"./index.28ca8494.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
